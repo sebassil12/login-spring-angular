50b75dd4bf4b061298b5bda2c4c964677e247f81
sebassil12 <anddysilva15@gmail.com>
2023-12-16 13:52:49 -0500

    Handle information between components

--- login-frontend/src/app/services/auth/login.service.ts
+++ login-frontend/src/app/services/auth/login.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { LoginRequest } from './LoginRequest';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, throwError, BehaviorSubject, tap } from 'rxjs';
 import { User } from './User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  currentUserLoginOn:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentUserData:BehaviorSubject<User> = new BehaviorSubject<User>({id:0, email:''});
 
   constructor(private http:HttpClient) { }
 
   login(credentials:LoginRequest):Observable<User>{
     return this.http.get <User> ("../../../assets/data.json").pipe(
+      tap( userData =>{
+        this.currentUserData.next(userData);
+        this.currentUserLoginOn.next(true);
+      }),
       catchError(this.handleError)
-    )
+    );
   }
 
   private handleError(error:HttpErrorResponse){
@@ -27,5 +33,13 @@ export class LoginService {
     }
     return throwError(()=>new Error("Algo fallo, intente nuevamente"))
 
+  }
+
+  get userData():Observable<User>{
+    return this.currentUserData.asObservable();
+  }
+
+  get userLoginOn():Observable<boolean>{
+    return this.currentUserLoginOn.asObservable();
   }
 }
